e2ed1f5f9459b91ee2ab30e878f193e149e7db09
Zayne Chuah <zheng_hung@hotmail.com>
2018-01-13 17:52:06 -0500

    Rearranged frames

--- Python/calWindow.py
+++ Python/calWindow.py
@@ -16,94 +16,130 @@ import matplotlib.pyplot as plt
 
 
 global cs_config, ser
+class calMode():
+	def __init__():
+		self.cal_start(ser, cs_config)
 
-def cal_start(ser, cs_config):
-	global status_str, calType_str
-	if ser==0:
-		cs_config = cs_config.get()
+	def cal_start(ser, cs_config):
+		global status_str, calType_str, cal_window
+		if ser==0:
+			cs_config = cs_config.get()
 
-	# Setup Window
-	cal_window = Tk()
-	cal_window.title("CAL Mode")
-	# cal_window.geometry('300x200')
+		# Setup Window
+		cal_window = Tk()
+		cal_window.title("CAL Mode")
+		# cal_window.geometry('300x200')
 
-	# Frame Containers
-	titleFrame = Frame(cal_window)
-	titleFrame.grid(row=0, column=0)	
-	stsFrame = Frame(cal_window)
-	stsFrame.grid(row=1, column=0, sticky='nw', padx=10)
-	mainUIFrame = Frame(cal_window)
-	mainUIFrame.grid(row=2, column=0, sticky='w', padx=10)
-	calOptionFrame = Frame(mainUIFrame)
-	calOptionFrame.grid(row=0, column=0, sticky='w', padx=10)
-	insFrame = Frame(mainUIFrame)
-	insFrame.grid(row = 0, column = 1, sticky ='w', padx = 10)
-	graphFrame = Frame(cal_window)
-	graphFrame.grid(row = 3, column = 0, sticky = 'w', padx =10)
+		# Frame Containers
+		titleFrame = Frame(cal_window)
+		titleFrame.grid(row=0, column=0)	
+		stsFrame = Frame(cal_window, borderwidth=3, relief=GROOVE)
+		stsFrame.grid(row=1, column=0, sticky='nsew', padx=10)
+		mainUIFrame = Frame(cal_window, borderwidth=3, relief=GROOVE)
+		mainUIFrame.grid(row=2, column=0, sticky='nsew', padx=10)
+		comFrame = Frame(cal_window, borderwidth=3, relief=GROOVE)	
+		comFrame.grid(row = 3, column = 0, sticky = 'nsew', padx = 10)
+		moiFrame = Frame(cal_window, borderwidth=3, relief=GROOVE)
+		moiFrame.grid(row = 4, column = 0, sticky = 'nsew', padx = 10)
 
-	# layout all of the main containers
-	cal_window.grid_rowconfigure(1, weight=1)
-	cal_window.grid_columnconfigure(0, weight=1)
+		# Frames within mainUIFrame Containers
+		calOptionFrame = Frame(mainUIFrame)
+		calOptionFrame.grid(row=0, column=0, sticky='nsew', padx=10)
+		insFrame = Frame(mainUIFrame)
+		insFrame.grid(row = 0, column = 1, sticky ='nsew', padx = 10)
 
-	# Title Label
-	cal_label = Label(titleFrame, text="Calibration Mode", font='Helvetica 16 bold')
-	cal_label.grid(row=0, column=0)
+		# Frames within comFrame Container
+		comFrameUI = Frame(comFrame)
+		comFrameUI.grid(row = 0, column = 0, sticky = 'nsew', padx =10)
+		graphFrame = Frame(comFrame, borderwidth=3, relief=GROOVE)
+		graphFrame.grid(row = 0, column = 1, sticky = 'e', padx=10)
 
-	# Status Window
-	sts_label = Label(stsFrame, text='Status Message: ', justify='left')
-	sts_label.grid(row=0,column=0, sticky='w')
-	status_str = StringVar(cal_window)
-	status_str.set('Place Calibration Block to begin')
-	status_label = Label(stsFrame, textvariable=status_str, justify='left', anchor=NW, font='Arial 10 italic', fg='gray', bd=2, relief='sunken')
-	status_label.config(height=3, width=80, wraplength=640)
-	status_label.grid(row=1, column=0, sticky='nw', pady=10)
-
-	# OptionMenu
-	calType_str = StringVar(cal_window)
-	calType_str.set('COM')
-	calTypeLbl = Label(calOptionFrame, text='Calibration Type: ')
-	calTypeLbl.grid(row = 0, column = 0, sticky = 'w')
-	calType = OptionMenu(calOptionFrame, calType_str, "MOI", "COM")
-	calType.grid(row = 0, column = 1)
-	
-	# Instruction Labels
-	insLabel = Label(
-		insFrame, 
-		text='1. Place CubeSat on the plate\n'
-		'2. Select the Calibration type and Begin calibration measurement', justify='left')	
-	insLabel.grid(row = 0, column =3, sticky='w')
-
-	plotCube(graphFrame)
+		# Frames within moiFrame Container
 
 
-	# Activate the window.
-	cal_window.mainloop()
+		# layout all of the main containers
+		cal_window.grid_rowconfigure(1, weight=1)
+		cal_window.grid_columnconfigure(0, weight=1)
 
-from mpl_toolkits.mplot3d import Axes3D
-import matplotlib.pyplot as plt
-import numpy as np
-from itertools import product, combinations
+		# Title Label
+		cal_label = Label(titleFrame, text="Calibration Mode", font='Helvetica 16 bold')
+		cal_label.grid(row=0, column=0)
 
-def plotCube(graphFrame):
+		# Status Window
+		sts_label = Label(stsFrame, text='Status Message: ', justify='left')
+		sts_label.grid(row=0,column=0, sticky='w')
+		status_str = StringVar(cal_window)
+		status_str.set('Place Calibration Block to begin')
+		status_label = Label(stsFrame, textvariable=status_str, justify='left', anchor=NW, font='Arial 10 italic', fg='gray', bd=2, relief='sunken')
+		status_label.config(height=3, width=100, wraplength=640)
+		status_label.grid(row=1, column=0, sticky='nw', pady=10, padx=5)
+		print(stsFrame.winfo_width())
+
+		# OptionMenu
+		calType_str = StringVar(cal_window)
+		calType_str.set('COM')
+		calTypeLbl = Label(calOptionFrame, text='Calibration Type: ')
+		calTypeLbl.grid(row = 0, column = 0, sticky = 'w')
+		calType = OptionMenu(calOptionFrame, calType_str, "MOI", "COM")
+		calType.grid(row = 0, column = 1)
+		
+		# Instruction Labels
+		insLabel = Label(
+			insFrame, 
+			text='1. Place CubeSat on the plate\n'
+			'2. Select the Calibration type and Begin calibration measurement', justify='left')	
+		insLabel.grid(row = 0, column = 3, sticky='w')
+
+
+		# COM Section
+		calMode.plotCube(graphFrame)
+		comlbl = Label(comFrameUI, text='Center of Mass')
+		comlbl.grid(row=0, column=0)
+
+
+		# MOI Section
+		moilbl = Label(moiFrame, text='Moment of Inertia')
+		moilbl.grid(row=0, column=0)
 
 
 
-
-	fig = plt.figure()
-	ax = fig.gca(projection='3d')
-	ax.set_aspect("equal")
+		saveButton = Button(cal_window, text='Save and Quit', command=calMode.save)
+		saveButton.grid(row=5, column=0, sticky='e', padx=10, pady=10)
 
 
-	# draw cube
-	r = [0, 15]
-	for s, e in combinations(np.array(list(product(r, r, r))), 2):
-	    if np.sum(np.abs(s-e)) == r[1]-r[0]:
-	        ax.plot3D(*zip(s, e), color="b")
-
-	canvas = FigureCanvasTkAgg(fig,master=graphFrame)
-	canvas.show()
-	canvas.get_tk_widget().pack(side='top', fill='both', expand=1)
-	ax.mouse_init()
+		# Activate the window.
+		cal_window.mainloop()
 
 
-cal_start(1, 'COM')
+	def plotCube(graphFrame):
+		from mpl_toolkits.mplot3d import Axes3D
+		import matplotlib.pyplot as plt
+		import numpy as np
+		from itertools import product, combinations
+
+		fig = plt.figure()
+		ax = fig.gca(projection='3d')
+		ax.set_aspect("equal")
+
+		cubeWidth = 15;
+
+		# draw cube
+		r = [0, cubeWidth]
+		for s, e in combinations(np.array(list(product(r, r, r))), 2):
+		    if np.sum(np.abs(s-e)) == r[1]-r[0]:
+		        ax.plot3D(*zip(s, e), color="b")
+
+	    # draw a point
+		ax.scatter([cubeWidth/2], [cubeWidth/2], [cubeWidth/2], color="r", s=3)
+
+		canvas = FigureCanvasTkAgg(fig, master=graphFrame)
+		canvas.show()
+		canvas.get_tk_widget().pack(side='top', fill='both', expand=1)
+
+		ax.mouse_init()
+
+
+	def save():
+		cal_window.destroy()
+
+# calMode.cal_start(1, 'COM')
