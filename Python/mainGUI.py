d1e4458803c862551583f0530c009df0d55e2b2f
Zayne Chuah <zheng_hung@hotmail.com>
2018-01-14 19:31:22 -0500

    Rewritten frame structure for consistency

--- Python/mainGUI.py
+++ Python/mainGUI.py
@@ -1,101 +1,106 @@
 from tkinter import *
-import comWindow
-import moiWindow
-import calWindow
-# -----------------------------------------------------------------------------
-# To use matplotlib, the author must use the TkAgg backend, or none of this will
-# work and a long string of inexplicable error messages will ensue.
-# -----------------------------------------------------------------------------
-import matplotlib
-matplotlib.use('TkAgg')
-import numpy as np
-import matplotlib.pyplot as plt
 import serial
+import calWindow
+import moiWindow
+import comWindow
 
-global serialConnect, cs_config, ser
+global serialConnect, cs_config, ser, globalstatus
 ser = 0
-
-# Define a bold font:
 BOLD = ('Helvetica', '24', 'bold')
 
-# Create main application window.
-root = Tk()
-root.title('CubeSat Testing Device')
-
-# Create a text box explaining the application.
-greeting = Label(text="CubeSat Testing Device", font=BOLD)
-greeting.pack(side='top')
-
-# Create a frame for variable names and entry boxes for their values.
-frametop = Frame(root)
-frametop.pack(side='top')
-frame = Frame(frametop)
-frame.pack(side='left')
-frame2 = Frame(frametop)
-frame2.pack(side='right')
-frame21 = Frame(frame2)
-frame21.pack(side='top')
-
-
-
-# Variables for the calculation, and default values.
-row_counter = 0
-
+# Button Functions
+# --------------------------
 # Connect to Arduino
 def serialConnect(event=None):
     global ser
     try:
         ser = serial.Serial('COM3', 9600)
-        connect = Label(frame, text="Success", font='Calibri 12 bold', fg='green')
+        connect = Label(controlFrame, text="Success", font='Calibri 12 bold', fg='green')
         connect.grid(row=0, column=1)
     except serial.SerialException: 
-        connect = Label(frame, text="Failed", font='Calibri 12 bold', fg='Red')
+        connect = Label(controlFrame, text="Failed", font='Calibri 12 bold', fg='Red')
         connect.grid(row=0, column=1)
-
-
-
-# Arduino Button
-arduino = Button(frame, command=serialConnect, text="Connect to Arduino")
-arduino.grid(row=row_counter, column=0, padx=5)
-
-row_counter+=1
-cs_text = Label(frame, text='CubeSat Config:')
-cs_text.grid(row=row_counter, column=0)
-
-cs_config = StringVar(root)
-cs_config.set("3U") # default value
-
-w = OptionMenu(frame, cs_config, "1U", "2U", "3U")
-w.grid(row = row_counter, column = 1)
-
-
-ins_text = Label(frame21, text='1. Place the CubeSat onto the platform with the predefined orientation\n'
-                               '2. Ensure the fixturing is properly clamping the CubeSat\n'
-                               '3. Select the appropriate config and Mode of operation', justify = 'left')
-ins_text.grid(row = 0, column = 0, padx=10)
-
 
 # Calibration Mode
 def cal_mode(event=None):
     global cellA, cellB, cellC, timer
     calWindow.calMode.cal_start(ser, cs_config)
 
+# ---------------------------
+
+# Create main application window.
+root = Tk()
+root.title('CubeSat Testing Device')
+
+# Frame Containers
+titleFrame = Frame(root)
+titleFrame.grid(row=0)
+mainUIFrame = Frame(root, borderwidth=3, relief=GROOVE)
+mainUIFrame.grid(row=1, column=0, sticky='nsew', padx=10)
+tabFrame = Frame(root)
+tabFrame.grid(row=2)
+
+# SubFrame Containers
+controlFrame = Frame(mainUIFrame, borderwidth=3, relief=GROOVE)
+controlFrame.grid(row=0, column=0, sticky='nsew', padx=10)
+instructionFrame = Frame(mainUIFrame, borderwidth=3, relief=GROOVE)
+instructionFrame.grid(row=0, column=1, sticky='nsew', padx=(10, 10))
+stsFrame = Frame(mainUIFrame, borderwidth=3, relief=GROOVE)
+stsFrame.grid(row=0, column=2, sticky='nsew', padx=10)
+
+# Title
+titleLabel = Label(titleFrame, text="CubeSat Testing Device", font=BOLD)
+titleLabel.grid(row=0)
+
+# Arduino Button
+arduino = Button(controlFrame, command=serialConnect, text="Connect to Arduino")
+arduino.grid(row=0, column=0, padx=5, sticky='nsew')
+
+# CubeSat Config selection
+cs_text = Label(controlFrame, text='CubeSat Config:')
+cs_text.grid(row=1, column=0)
+cs_config = StringVar(root)
+cs_config.set("3U") # default value
+w = OptionMenu(controlFrame, cs_config, "1U", "2U", "3U")
+w.grid(row = 1, column = 1, sticky='nsew')
+
 # Calibration Button
-Mode_COM = Button(root, command = cal_mode, text="Calibrate Measurements")
-Mode_COM.pack(side='top', fill='both', padx=5, pady=2)
+calButton = Button(controlFrame, command = cal_mode, text="Calibrate Measurements")
+calButton.grid(row=2, columnspan=2, sticky='nsew')
+
+# Instructions Label
+ins_text = Label(instructionFrame, text=
+    '1. Place the CubeSat onto the platform with the predefined orientation\n'
+    '2. Ensure the fixturing is properly clamping the CubeSat\n'
+    '3. Select the appropriate config and Mode of operation\n'
+    '4. Calibrate Measurements with the specified block for first time measurements'
+    , justify = 'left')
+ins_text.config(width=60)
+ins_text.grid(row = 0, column = 0, padx=5, sticky='nsew')
+
+#Status Label
+stslbl = Label(stsFrame, text='Status Message')
+stslbl.grid(row=0, padx=10, pady=5, sticky='nw')
+globalstatus = StringVar(root)
+globalstatus.set('Place CubeSat to begin')
+status_label = Label(stsFrame, textvariable=globalstatus, justify='left', anchor=NW, font='Arial 10 italic', fg='gray', bd=2, relief='sunken')
+status_label.config(height = 2, width=50, wraplength=320)
+status_label.grid(row=1, padx=10, pady=5, sticky='nsew')   
+
+
 
 # Notebook TabsView
 from tkinter import ttk
-tabsView = ttk.Notebook(root)
+tabsView = ttk.Notebook(tabFrame)
 comFrame = Frame(tabsView)
 moiFrame = Frame(tabsView)
 tabsView.add(comFrame, text='Center of Mass')
 tabsView.add(moiFrame, text='Moment of Inertia')
-tabsView.pack(side='top', fill='both', padx=5, pady=5)
+tabsView.pack(side='top', fill='both', padx=0, pady=5)
 
 # Setup COM and MOI Mode
 comWindow.comMode.com_start(ser, cs_config, comFrame)
-moiWindow.moi_start(ser, cs_config, moiFrame)
+moiWindow.moiMode.moi_start(ser, cs_config, moiFrame)
 
 
 # Allow pressing <Esc> to close the window.
@@ -103,6 +108,9 @@ root.bind('<Escape>', lambda e: root.quit())
 
 # The window is not resizable. 
 root.resizable(0,0) 
+
+# print ('height:',root.winfo_height())
+# print ('width:',root.winfo_width())
 
 # Activate the window.
 root.mainloop()
