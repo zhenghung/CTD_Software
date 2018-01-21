7dc94d66f1ff31b177397f0ee95ca6f43f5a769e
Zayne Chuah <zheng_hung@hotmail.com>
2018-01-21 15:03:22 -0500

    COM and MOI State SerialRead and prints

--- Arduino/ArduinoCTD/ArduinoCTD.ino
+++ Arduino/ArduinoCTD/ArduinoCTD.ino
@@ -1,8 +1,9 @@
-enum state_enum {COM_STANDBY, COM_M1, COM_M2, MOI_STANDBY, MOI_M1, MOI_M2, MOI_M3};
-enum action_enum {CHGMOI, CHGCOM, BEGIN_COM1, BEGIN_COM2, COM_DONE, BEGIN_MOI1, BEGIN_MOI2, BEGIN_MOI3, MOI_DONE, RESET};
+enum state_enum {STARTUP, COM_STANDBY, COM_M1, COM_M2, MOI_STANDBY, MOI_M1, MOI_M2, MOI_M3};
+enum action_enum {CHGCOM, BEGIN_COM1, BEGIN_COM2, COM_DONE, CHGMOI, BEGIN_MOI1, BEGIN_MOI2, BEGIN_MOI3, MOI_DONE, RESET};
 
 int serialInput;
-state_enum state = COM_STANDBY;
+int firstLaunch = true;
+state_enum state = STARTUP;
 action_enum action;
 
 #define loadcellA A0
@@ -10,6 +11,11 @@ action_enum action;
 #define loadcellC A2
 #define tachometer A3
 #define led 10
+
+// DEFINING SERIAL INPUTS
+//#define CHGCOM 'Q'
+//#define CHGMOI 'A'
+//#define BEGIN_COM1
 
 void setup() {
   pinMode(loadcellA, INPUT);
@@ -27,15 +33,124 @@ void loop() {
 
 void state_machine(uint8_t action){
   switch(state){
+    case STARTUP:
+      if(firstLaunch){
+        Serial.println("STARTUP");
+        firstLaunch = false;
+      }
+      if(action == CHGCOM){
+        state = COM_STANDBY;
+        Serial.println("CHGCOM");
+      }
+      if(action == CHGMOI){
+        state = MOI_STANDBY;
+        Serial.println("MOI_STANDBY");
+      }
+      break;
     case COM_STANDBY:
       if(action == CHGMOI){
         state = MOI_STANDBY;
-        Serial.println("moichged");
+        Serial.println("CHGMOI");
       }
       else if(action == BEGIN_COM1){
         state = COM_M1;
+        Serial.println("BEGIN_COM1");
         com_measure(0);
       }
+      else if(action == RESET){
+        state = COM_STANDBY;
+        Serial.println("RESET");
+      }
+      else{
+        
+      }
+      break;
+    case COM_M1:
+      if(action == BEGIN_COM2){
+        state = COM_M2;
+        Serial.println("BEGIN_COM2");
+        com_measure(1);        
+      }
+      else if(action == RESET){
+        state = COM_STANDBY;
+        Serial.println("RESET");
+      }
+      else{
+        
+      }
+      break;
+    case COM_M2:
+      if(action == COM_DONE){
+        state = COM_STANDBY;
+        Serial.println("COM_DONE");       
+      }
+      else if(action == RESET){
+        state = COM_STANDBY;
+        Serial.println("RESET");
+      }
+      else{
+        
+      }
+      break;
+    case MOI_STANDBY:
+      if(action == CHGCOM){
+        state = COM_STANDBY;
+        Serial.println("CHGCOM");
+      }
+      else if(action == BEGIN_MOI1){
+        state = MOI_M1;
+        Serial.println("BEGIN_MOI1");
+        moi_measure(0);
+      }
+      else if(action == RESET){
+        state = MOI_STANDBY;
+        Serial.println("RESET");
+      }
+      else{
+        
+      }
+      break;
+    case MOI_M1:
+      if(action == BEGIN_MOI2){
+        state = MOI_M2;
+        Serial.println("BEGIN_MOI2");
+        moi_measure(1);        
+      }
+      else if(action == RESET){
+        state = MOI_STANDBY;
+        Serial.println("RESET");
+      }
+      else{
+        
+      }
+      break;
+    case MOI_M2:
+      if(action == BEGIN_MOI3){
+        state = MOI_M3;
+        Serial.println("BEGIN_MOI3");
+        moi_measure(2);        
+      }
+      else if(action == RESET){
+        state = MOI_STANDBY;
+        Serial.println("RESET");
+      }
+      else{
+        
+      }
+      break;
+    case MOI_M3:
+      if(action == MOI_DONE){
+        state = MOI_STANDBY;
+        Serial.println("MOI_DONE");        
+      }
+      else if(action == RESET){
+        state = MOI_STANDBY;
+        Serial.println("RESET");
+      }
+      else{
+        
+      }
+      break;      
   }
 }
 
@@ -43,34 +158,34 @@ uint8_t listeningLoop(){
   while(Serial.available()>0){
     serialInput = Serial.read();
     switch(serialInput){
-      case 'M0':
-        return CHGMOI;
-        break;
-      case 'C0':
+      case 'Q':
         return CHGCOM;
         break;
-      case 'C1':
+      case 'W':
         return BEGIN_COM1;
         break;
-      case 'C2':
+      case 'E':
         return BEGIN_COM2;
         break;  
-      case 'C9':
+      case 'R':
         return COM_DONE;
         break;  
-      case 'M1':
+      case 'A':
+        return CHGMOI;
+        break;
+      case 'S':
         return BEGIN_MOI1;
         break;
-      case 'M2':
+      case 'D':
         return BEGIN_MOI2;
         break;
-      case 'M3':
+      case 'F':
         return BEGIN_MOI3;
         break;
-      case 'M9':
+      case 'G':
         return MOI_DONE;
         break;  
-      case 'RS':
+      case '0':
         return RESET;
         break;
     }
