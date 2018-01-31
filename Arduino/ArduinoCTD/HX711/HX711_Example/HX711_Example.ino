97774ed95d5055063b36969fe135d5ce146ccacf
Zayne Chuah <zheng_hung@hotmail.com>
2018-01-31 00:40:43 -0500

    Testing with different calibration settings

--- Arduino/ArduinoCTD/HX711/HX711_Example/HX711_Example.ino
+++ Arduino/ArduinoCTD/HX711/HX711_Example/HX711_Example.ino
@@ -10,47 +10,47 @@ void setup() {
   // parameter "gain" is ommited; the default value 128 is used by the library
   // HX711.DOUT	- pin #A1
   // HX711.PD_SCK	- pin #A0
-  scale.begin(7, 6);
-//
-//  Serial.println("Before setting up the scale:");
-//  Serial.print("read: \t\t");
-//  Serial.println(scale.read());			// print a raw reading from the ADC
-//
-//  Serial.print("read average: \t\t");
-//  Serial.println(scale.read_average(20));  	// print the average of 20 readings from the ADC
-//
-//  Serial.print("get value: \t\t");
-//  Serial.println(scale.get_value(5));		// print the average of 5 readings from the ADC minus the tare weight (not set yet)
-//
-//  Serial.print("get units: \t\t");
-//  Serial.println(scale.get_units(5), 1);	// print the average of 5 readings from the ADC minus tare weight (not set) divided
-//						// by the SCALE parameter (not set yet)
+  scale.begin(3, 2);
 
-  scale.set_scale(477.f);                      // this value is obtained by calibrating the scale with known weights; see the README for details
+  Serial.println("Before setting up the scale:");
+  Serial.print("read: \t\t");
+  Serial.println(scale.read());			// print a raw reading from the ADC
+
+  Serial.print("read average: \t\t");
+  Serial.println(scale.read_average(20));  	// print the average of 20 readings from the ADC
+
+  Serial.print("get value: \t\t");
+  Serial.println(scale.get_value(5));		// print the average of 5 readings from the ADC minus the tare weight (not set yet)
+
+  Serial.print("get units: \t\t");
+  Serial.println(scale.get_units(5), 1);	// print the average of 5 readings from the ADC minus tare weight (not set) divided
+						// by the SCALE parameter (not set yet)
+
+  scale.set_scale(440.f);                      // this value is obtained by calibrating the scale with known weights; see the README for details
   scale.tare();				        // reset the scale to 0
-//
-//  Serial.println("After setting up the scale:");
-//
-//  Serial.print("read: \t\t");
-//  Serial.println(scale.read());                 // print a raw reading from the ADC
-//
-//  Serial.print("read average: \t\t");
-//  Serial.println(scale.read_average(20));       // print the average of 20 readings from the ADC
-//
-//  Serial.print("get value: \t\t");
-//  Serial.println(scale.get_value(5));		// print the average of 5 readings from the ADC minus the tare weight, set with tare()
-//
-//  Serial.print("get units: \t\t");
-//  Serial.println(scale.get_units(5), 1);        // print the average of 5 readings from the ADC minus tare weight, divided
-//						// by the SCALE parameter set with set_scale
-//
-//  Serial.println("Readings:");
+
+  Serial.println("After setting up the scale:");
+
+  Serial.print("read: \t\t");
+  Serial.println(scale.read());                 // print a raw reading from the ADC
+
+  Serial.print("read average: \t\t");
+  Serial.println(scale.read_average(20));       // print the average of 20 readings from the ADC
+
+  Serial.print("get value: \t\t");
+  Serial.println(scale.get_value(5));		// print the average of 5 readings from the ADC minus the tare weight, set with tare()
+
+  Serial.print("get units: \t\t");
+  Serial.println(scale.get_units(5), 1);        // print the average of 5 readings from the ADC minus tare weight, divided
+						// by the SCALE parameter set with set_scale
+
+  Serial.println("Readings:");
 }
 
 void loop() {
   Serial.print("one reading:\t");
 //  float value= scale.get_units(10) + 0.3;
-  Serial.println(scale.get_units(10), 2);
+  Serial.println(scale.get_units(5), 2);
 //  Serial.println(value, 5);
 //  Serial.print("\t| average:\t");
 //  Serial.println(scale.get_units(10), 1);
